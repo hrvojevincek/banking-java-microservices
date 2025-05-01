cf08351e87928112c2aaf5374c5df8d2703fb7a9
hrvoje vincek <vincekhrvoje@gmail.com>
2025-05-01 19:47:25 +0200

    initial commit- backend

---
+++ userservice/src/main/java/com/banking/userservice/model/User.java
@@ -0,0 +1,31 @@
+package com.banking.userservice.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+    private String firstName;
+    private String lastName;
+
+    @Column(nullable = false)
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
